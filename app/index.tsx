64178b13c1c1eaf77a69d9b52a89e8a665c893d9
Nistank <nickstank@hotmail.com>
2024-09-25 18:34:02 -0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,70 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function Home() {
+const PaginaInicial = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Bem-vindo ao App!</Text>
-      <Link href="/cadastrobar">Ir para o Cadastro do Bar</Link>
-      <Link href="/cadastrousuario">Ir para o Cadastro do Usuário</Link>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao App!</Text>
+
+      <Link href="/entrar" style={styles.button}>Entrar</Link>
+        <View style={styles.buttonContainer}>
+        <Link href="/cadastrousuario" style={styles.button}>
+          <Text style={styles.buttonText}>Cadastre-se</Text>
+        </Link>
+        <Link href="/cadastrobar" style={styles.button}>
+          <Text style={styles.buttonText}>Cadastre seu Bar</Text>
+        </Link>
+        <Link href="/sobreoapp">
+          <Text style={styles.buttonText}>Ir para Sobre o Aplicativo</Text>
+        </Link>
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "#000",
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    gap: 10
+  ,
+  },
+  button: {
+    color: '#fff',
+    textAlign: 'center',
+    backgroundColor: '#8c7b47',
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 20,
+    borderRadius: 5,
+    width: 200,
+    padding: 10
+  
+
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    backgroundColor: '#8c7b47',
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+});
+
+export default PaginaInicial;
